09158cab05d627a5632181695232d4598ff68087
Taiga Yamada <taiga06091998@icloud.com>
2025-05-29 06:23:06 +0900

    コード更新

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: "/Save-Money_TypeScript/",
+  publicDir: 'public',
   plugins: [react()],
 })
